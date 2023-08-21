6169818ad09e4b6229cfdf0b2efd7624b5e40334
lubshad <lubshad4u4@gmail.com>
2023-08-22 02:20:14 +0530

    asset included

--- import_starter.sh
+++ import_starter.sh
@@ -1,8 +1,8 @@
 git clone https://github.com/lubshad/starter.git &&
 cp -r starter/lib . &&
+cp -r starter/assets . &&
 rm -rf starter &&
-echo "
-  dio: ^5.3.2
+echo 'dio: ^5.3.2
   dartz: ^0.10.1
   flutter_spinkit: ^5.2.0
   pinput: ^3.0.0
@@ -11,4 +11,5 @@ echo "
   firebase_core: ^2.15.1
   firebase_analytics: ^10.4.5
   flutter_svg: ^2.0.7
-  animations: ^2.0.7" >> pubspec.yaml
+  animations: ^2.0.7' >> pubspec.yaml
+
