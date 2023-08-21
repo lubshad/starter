d222e13b6e4bffe35a81fda70e6a3970b211379e
lubshad <lubshad4u4@gmail.com>
2023-08-22 02:06:42 +0530

    importer updated

--- import_starter.sh
+++ import_starter.sh
@@ -7,4 +7,9 @@ echo "
   flutter_spinkit: ^5.2.0
   pinput: ^3.0.0
   country_code_picker: ^3.0.0
-  firebase_auth: ^4.7.2" >> pubspec.yaml
+  firebase_auth: ^4.7.2
+  firebase_core: ^2.15.1
+  firebase_analytics: ^10.4.5
+  flutter_svg: ^2.0.7
+  animations: ^2.0.7"
+>> pubspec.yaml
