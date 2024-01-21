ca64acf17cd2d76e5855e90045a29f4ea72a9df0
lubshad <lubshad4u4@gmail.com>
2024-01-21 14:45:19 +0530

    sized box changed to gap

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,22 +1,23 @@
-import 'package:flutter/material.dart';
+
+import 'package:gap/gap.dart';
 
 delayed({Duration duration = const Duration(seconds: 2)}) async {
   return await Future.delayed(duration);
 }
 
 const double padding = 8;
-const double paddingSmall = padding / 2;
-const double paddingLarge = padding * 2;
-const double paddingXL = padding * 4;
-const double paddingTiny = paddingSmall / 2;
+const double paddingLarge = 16;
+const double paddingXL = 32;
+const double paddingXXL = 64;
+const double paddingSmall =  4;
+const double paddingTiny =  2;
 
-const gap = SizedBox(height: padding);
-const gapXL = SizedBox(height: paddingXL);
-const gapLarge = SizedBox(height: paddingLarge);
-const gapSmall = SizedBox(height: paddingSmall);
-const gapHorizontal = SizedBox(width: padding);
-const gapHorizontalLarge = SizedBox(width: paddingLarge);
-const gapHorizontalSmall = SizedBox(width: paddingSmall);
+const gap = Gap(padding);
+const gapLarge = Gap(paddingLarge);
+const gapXL = Gap(paddingXL);
+const gapXXL = Gap(paddingXXL);
+const gapSmall = Gap(paddingSmall);
+const gapTiny = Gap(paddingTiny);
 
 const Duration animationDuration = Duration(milliseconds: 300);
 const Duration animationDurationLarge = Duration(seconds: 1);
