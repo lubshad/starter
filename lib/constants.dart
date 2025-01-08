78161f835bb5de752b400f64ca722929bcc903ab
lubshad <lubshad4u4@gmail.com>
2025-01-08 18:34:49 +0530

    date today yesteday tomorrom

--- lib/constants.dart
+++ lib/constants.dart
@@ -85,6 +85,21 @@ extension DateTimeExtension on DateTime? {
       this == null ? null : DateFormat("yyyy-MM-dd").format(this!);
   String? get dateTimeFormat =>
       this == null ? null : DateFormat("E, MMM, d, y, hh:mm aa").format(this!);
-  String? get dateFormat =>
-      this == null ? null : DateFormat("E, MMM, d, y").format(this!);
+  String? get dateFormat {
+    if (this == null) return null;
+
+    DateTime today = DateTime.now().copyWith(hour: 0, minute: 0, second: 0);
+    int daysDifference =
+        today.difference(this!.copyWith(hour: 0, minute: 0, second: 0)).inDays;
+    if (daysDifference == 1) {
+      return "Yesterday";
+    }
+    if (daysDifference == -1) {
+      return "Tomorrow";
+    }
+    if (daysDifference == 0) {
+      return "Today";
+    }
+    return DateFormat("E, MMM, d, y").format(this!);
+  }
 }
