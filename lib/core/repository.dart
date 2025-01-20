f19efd20c6cf185a84f8e7dff5a0c2ef802457e6
lubshad <lubshad4u4@gmail.com>
2025-01-20 12:43:20 +0530

    token auth interceptor added

--- lib/core/repository.dart
+++ lib/core/repository.dart
@@ -3,6 +3,7 @@
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:get/utils.dart';
+import 'package:starter/features/authentication/phone_auth/phone_auth_mixin.dart';
 import '../features/profile_screen/profile_details_model.dart';
 import '../models/name_id.dart';
 import '../services/shared_preferences_services.dart';
@@ -34,7 +35,7 @@ class DataRepository with ErrorExceptionHandler {
   DataRepository._private() {
     var cookieJar = CookieJar(ignoreExpires: false);
     _client.interceptors.add(CookieManager(cookieJar));
-    _client.interceptors.add(AuthenticationInterceptor());
+    _client.interceptors.add(TokenAuthInterceptor());
     _client.interceptors.add(LoggingInterceptor());
   }
 
