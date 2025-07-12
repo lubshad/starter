93534fe79cc8b132cb8a55e63efb032ab2bcf3e9
lubshad <lubshad4u4@gmail.com>
2025-07-12 12:19:10 +0530

    qrscanner added

---
+++ lib/features/qr_scanner/face_overlay.dart
@@ -0,0 +1,142 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+import '../../exporter.dart';
+
+class FaceScanOverlay extends StatefulWidget {
+  const FaceScanOverlay({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _FaceScanOverlayState createState() => _FaceScanOverlayState();
+}
+
+class _FaceScanOverlayState extends State<FaceScanOverlay>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _scanAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..repeat(reverse: true);
+
+    _scanAnimation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final frameHeight = SizeUtils.width * .8;
+    final frameWidth = SizeUtils.width * .7;
+
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        LayoutBuilder(
+          builder: (context, constraints) {
+            return CustomPaint(
+              size: Size(constraints.maxWidth, constraints.maxHeight),
+              painter: FaceOverlayPainter(
+                frameHeight: frameHeight,
+                frameWidth: frameWidth,
+              ),
+            );
+          },
+        ),
+        SizedBox(
+          height: frameHeight,
+          child: AnimatedBuilder(
+            animation: _scanAnimation,
+            builder: (context, child) {
+              return Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    top: _scanAnimation.value * (frameHeight - 4),
+                  ),
+                  child: Container(
+                    width: frameWidth * 0.8,
+                    height: 4,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [Color(0xffD52358), Color(0xffEC7130)],
+                        stops: const [0.2, 0.5],
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0xffEC7130),
+                          blurRadius: 10,
+                          spreadRadius: 2,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(padding),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        Positioned(
+          bottom: 200,
+          left: 0,
+          right: 0,
+          child: Text(
+            "Align your face within the frame",
+            textAlign: TextAlign.center,
+            style: context.bodySmall.copyWith(color: Colors.white),
+          ),
+        ),
+        Positioned(
+          bottom: 170,
+          left: 0,
+          right: 0,
+          child: Center(
+            child: Text(
+              'Make sure your face is clearly visible',
+              style: context.bodyLarge.copyWith(color: Colors.white),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class FaceOverlayPainter extends CustomPainter {
+  final double frameWidth;
+  final double frameHeight;
+  FaceOverlayPainter({required this.frameWidth, required this.frameHeight});
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()..color = Colors.black.withAlpha((255 * .2).toInt());
+
+    final cutoutRect = Rect.fromCenter(
+      center: Offset(size.width / 2, size.height / 2),
+      width: frameWidth,
+      height: frameHeight,
+    );
+
+    final path = Path()
+      ..addRect(Rect.fromLTWH(0, 0, size.width, size.height))
+      ..addRRect(RRect.fromRectAndRadius(cutoutRect, const Radius.circular(20)))
+      ..fillType = PathFillType.evenOdd;
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
---
+++ lib/features/qr_scanner/qr_scanning_screen.dart
@@ -0,0 +1,69 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mlkit_barcode_scanning/google_mlkit_barcode_scanning.dart';
+
+import '../../exporter.dart';
+import 'barcode_detector_painter.dart';
+import 'detector_view.dart';
+
+class BarcodeScannerView extends StatefulWidget {
+  const BarcodeScannerView({super.key});
+
+  @override
+  State<BarcodeScannerView> createState() => _BarcodeScannerViewState();
+}
+
+class _BarcodeScannerViewState extends State<BarcodeScannerView> {
+  final BarcodeScanner _barcodeScanner = BarcodeScanner();
+  bool _canProcess = true;
+  bool _isBusy = false;
+  CustomPaint? _customPaint;
+  var _cameraLensDirection = SizeUtils.deviceType == DeviceType.mobile
+      ? CameraLensDirection.back
+      : CameraLensDirection.front;
+
+  @override
+  void dispose() {
+    _canProcess = false;
+    _barcodeScanner.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DetectorView(
+      customPaint: _customPaint,
+      onImage: _processImage,
+      initialCameraLensDirection: _cameraLensDirection,
+      onCameraLensDirectionChanged: (value) => _cameraLensDirection = value,
+    );
+  }
+
+  Future<void> _processImage(InputImage inputImage) async {
+    if (!_canProcess) return;
+    if (_isBusy) return;
+    _isBusy = true;
+    final barcodes = await _barcodeScanner.processImage(inputImage);
+    if (inputImage.metadata?.size != null &&
+        inputImage.metadata?.rotation != null) {
+      final painter = BarcodeDetectorPainter(
+        barcodes,
+        inputImage.metadata!.size,
+        inputImage.metadata!.rotation,
+        _cameraLensDirection,
+      );
+      _customPaint = CustomPaint(painter: painter);
+    } else {
+      _customPaint = null;
+    }
+    if (barcodes.isNotEmpty && mounted) {
+      await delayed(duration: animationDuration);
+    }
+    if (mounted) {
+      setState(() {});
+    }
+    _isBusy = false;
+  }
+}
