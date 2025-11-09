13345879d22e7e48241ad36f705f462822e02dc6
lubshad <lubshad4u4@gmail.com>
2025-11-09 13:30:58 +0530

    chatmixin updated

--- lib/features/chat/mixins/chat_mixin.dart
+++ lib/features/chat/mixins/chat_mixin.dart
@@ -1,17 +1,27 @@
 import 'package:agora_chat_uikit/chat_uikit.dart';
 
 import 'package:flutter/widgets.dart';
+import 'package:starter/core/repository.dart';
+import 'package:starter/features/chat/agora_rtm_service.dart';
 import '../../../core/app_route.dart';
 import '../../../main.dart';
 
 mixin ChatMixin {
   ValueNotifier<bool> buttonLoading = ValueNotifier(false);
-  void messageUser(String id, {BuildContext? context}) async {
+  void messageUser(
+    String id,
+    String name,
+    String profileImage) async {
     buttonLoading.value = true;
     ChatUIKitProfile? chatProfile = ChatUIKitProvider.instance.getProfileById(
       id,
     );
     if (chatProfile == null) {
+      await DataRepository.i.generateRTMToken(
+        username: id,
+        avatarUrl: profileImage,
+        nickname: name,
+      );
       final chatUserInfo =
           (await ChatClient.getInstance.userInfoManager.fetchUserInfoById([
             id,
@@ -24,8 +34,9 @@ mixin ChatMixin {
       );
     }
     ChatUIKitProvider.instance.addProfiles([chatProfile]);
+    buttonLoading.value = false;
     navigate(
-      context ?? navigatorKey.currentContext!,
+      navigatorKey.currentContext!,
       ChatUIKitRouteNames.messagesView,
       arguments: MessagesViewArguments(profile: chatProfile),
     );
