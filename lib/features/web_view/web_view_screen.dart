40efb2ea3b13e3e98f838a2075ca649cef9474df
lubshad <lubshad4u4@gmail.com>
2025-11-15 22:19:58 +0530

    confirmation added in webview

--- lib/features/web_view/web_view_screen.dart
+++ lib/features/web_view/web_view_screen.dart
@@ -7,7 +7,12 @@ import '../../widgets/custom_appbar.dart';
 class WebviewArgs {
   final String title;
   final String url;
-  WebviewArgs({required this.title, required this.url});
+  final bool confirmationNeeded;
+  WebviewArgs({
+    required this.title,
+    required this.url,
+    this.confirmationNeeded = false,
+  });
 }
 
 class WebViewScreen extends StatefulWidget {
@@ -22,16 +27,52 @@ class WebViewScreen extends StatefulWidget {
 }
 
 class _WebViewScreenState extends State<WebViewScreen> {
+  Future<bool> _onWillPop() async {
+    if (widget.arguments.confirmationNeeded) {
+      final shouldPop = await showDialog<bool>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Confirm'),
+            content: const Text('Are you sure you want to leave this page?'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(true),
+                child: const Text('Leave'),
+              ),
+            ],
+          );
+        },
+      );
+      return shouldPop ?? false;
+    }
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
     final webUri = WebUri(widget.arguments.url);
-    return Scaffold(
-      appBar: CustomAppBar(title: (widget.arguments.title)),
-      body: InAppWebView(
-        initialUrlRequest: URLRequest(url: webUri),
-        initialSettings: InAppWebViewSettings(
-          javaScriptEnabled: true,
-          supportZoom: false,
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (bool didPop, dynamic result) async {
+        if (didPop) return;
+        final shouldPop = await _onWillPop();
+        if (shouldPop && context.mounted) {
+          Navigator.of(context).pop();
+        }
+      },
+      child: Scaffold(
+        appBar: CustomAppBar(title: (widget.arguments.title)),
+        body: InAppWebView(
+          initialUrlRequest: URLRequest(url: webUri),
+          initialSettings: InAppWebViewSettings(
+            javaScriptEnabled: true,
+            supportZoom: false,
+          ),
         ),
       ),
     );
