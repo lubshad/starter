ae197dc0d48c50eb34d9e00ac50190c51ac702b4
lubshad <lubshad4u4@gmail.com>
2025-11-16 21:23:00 +0530

    location display name

--- lib/features/location_tracking/location_service.dart
+++ lib/features/location_tracking/location_service.dart
@@ -8,7 +8,6 @@ import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart' hide Location;
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
 import '../../core/app_route.dart';
 import '../../exporter.dart';
 import '../../main.dart';
@@ -21,21 +20,12 @@ import 'location_restriction_bottom_sheet.dart';
 @pragma('vm:entry-point')
 LocationSettings get locationSettings {
   if (defaultTargetPlatform == TargetPlatform.android) {
-    return AndroidSettings(
-      accuracy: LocationAccuracy.high,
-      distanceFilter: 10,
-    );
+    return AndroidSettings(accuracy: LocationAccuracy.high, distanceFilter: 10);
   } else if (defaultTargetPlatform == TargetPlatform.iOS ||
       defaultTargetPlatform == TargetPlatform.macOS) {
-    return AppleSettings(
-      accuracy: LocationAccuracy.high,
-      distanceFilter: 10,
-    );
+    return AppleSettings(accuracy: LocationAccuracy.high, distanceFilter: 10);
   } else if (kIsWeb) {
-    return WebSettings(
-      accuracy: LocationAccuracy.high,
-      distanceFilter: 10,
-    );
+    return WebSettings(accuracy: LocationAccuracy.high, distanceFilter: 10);
   } else {
     return LocationSettings(
       accuracy: LocationAccuracy.high,
@@ -68,15 +58,27 @@ class LocationService extends ChangeNotifier {
     );
   }
 
+  String get displayLocation =>
+      [null, ""].contains(placemarks.firstOrNull?.name)
+      ? "Unknown".tr
+      : placemarks.first.name!;
+
+  String get displayLocationName =>
+      [null, ""].contains(placemarks.firstOrNull?.locality)
+      ? "Unknown".tr
+      : placemarks.first.locality!;
+
   static void adjustCameraView(
-      GoogleMapController controller, LatLngBounds bounds) {
-    controller.animateCamera(
-      CameraUpdate.newLatLngBounds(bounds, 50),
-    );
+    GoogleMapController controller,
+    LatLngBounds bounds,
+  ) {
+    controller.animateCamera(CameraUpdate.newLatLngBounds(bounds, 50));
   }
 
   static Future<BitmapDescriptor> createCustomMarker(
-      Uint8List imageData, Color borderColor) async {
+    Uint8List imageData,
+    Color borderColor,
+  ) async {
     try {
       final ui.PictureRecorder pictureRecorder = ui.PictureRecorder();
       final Canvas canvas = Canvas(pictureRecorder);
@@ -93,23 +95,32 @@ class LocationService extends ChangeNotifier {
       final double imageRadius = (size / 2) - borderSize;
       final ui.Image image = await decodeImageFromList(imageData);
 
-      canvas.clipPath(Path()
-        ..addOval(Rect.fromCircle(
-            center: Offset(size / 2, size / 2), radius: imageRadius)));
+      canvas.clipPath(
+        Path()..addOval(
+          Rect.fromCircle(
+            center: Offset(size / 2, size / 2),
+            radius: imageRadius,
+          ),
+        ),
+      );
       paintImage(
         canvas: canvas,
         rect: Rect.fromCircle(
-            center: Offset(size / 2, size / 2), radius: imageRadius),
+          center: Offset(size / 2, size / 2),
+          radius: imageRadius,
+        ),
         image: image,
         fit: BoxFit.cover,
       );
 
       // Convert canvas to Image
-      final ui.Image finalImage = await pictureRecorder
-          .endRecording()
-          .toImage(size.toInt(), size.toInt());
-      final ByteData? byteData =
-          await finalImage.toByteData(format: ui.ImageByteFormat.png);
+      final ui.Image finalImage = await pictureRecorder.endRecording().toImage(
+        size.toInt(),
+        size.toInt(),
+      );
+      final ByteData? byteData = await finalImage.toByteData(
+        format: ui.ImageByteFormat.png,
+      );
       final Uint8List markerImage = byteData!.buffer.asUint8List();
 
       return BitmapDescriptor.bytes(
@@ -123,18 +134,18 @@ class LocationService extends ChangeNotifier {
   }
 
   // setupGeoTracking(LogType type) async {
-    // if (!Platform.isAndroid) return;
-    // // need to start only if autocheckout or livetracking is required
-    // if (!(CommonController.i.profileDetails!.employee.autoCheckout == true ||
-    //     CommonController.i.profileDetails!.employee.liveTracking == true)) {
-    //   return;
-    // }
+  // if (!Platform.isAndroid) return;
+  // // need to start only if autocheckout or livetracking is required
+  // if (!(CommonController.i.profileDetails!.employee.autoCheckout == true ||
+  //     CommonController.i.profileDetails!.employee.liveTracking == true)) {
+  //   return;
+  // }
 
-    // if (type == LogType.checkout) {
-    //   await FlutterForegroundTask.stopService();
-    // } else {
-    //   await FlutterForgroundTaskHelper.startService();
-    // }
+  // if (type == LogType.checkout) {
+  //   await FlutterForegroundTask.stopService();
+  // } else {
+  //   await FlutterForgroundTaskHelper.startService();
+  // }
   // }
 
   bool checkWithinRadius(GeofenceRegion geofenceRegion) {
@@ -180,22 +191,26 @@ class LocationService extends ChangeNotifier {
   Future<void> setCountryCode() async {
     if (placemarks.isEmpty) return;
 
-    countryCode =
-        CountryCode.fromCountryCode(placemarks.first.isoCountryCode ?? "IN");
+    countryCode = CountryCode.fromCountryCode(
+      placemarks.first.isoCountryCode ?? "IN",
+    );
     notifyListeners();
   }
 
   Future<void> setPlaceMarks() async {
     if (position == null) return;
-    placemarks =
-        await placemarkFromCoordinates(position!.latitude, position!.longitude);
+    placemarks = await placemarkFromCoordinates(
+      position!.latitude,
+      position!.longitude,
+    );
     setCountryCode();
   }
 
   Future<Position?> getCurrentPosition({bool direct = false}) async {
     if (direct) {
       return await Geolocator.getCurrentPosition(
-          locationSettings: locationSettings);
+        locationSettings: locationSettings,
+      );
     }
     final hasPermission = await _handlePermission();
 
@@ -203,19 +218,23 @@ class LocationService extends ChangeNotifier {
       return null;
     }
     position =
-        await Geolocator.getCurrentPosition(locationSettings: locationSettings)
-            .onError(
-      (error, stackTrace) {
-        logError(error);
-        return Future.error(error!);
-      },
-    );
+        await Geolocator.getCurrentPosition(
+          locationSettings: locationSettings,
+        ).onError((error, stackTrace) {
+          logError(error);
+          return Future.error(error!);
+        });
     setPlaceMarks();
     return position;
   }
 
-  bool isWithinRadius(double centerLat, double centerLng, double checkLat,
-      double checkLng, double radius) {
+  bool isWithinRadius(
+    double centerLat,
+    double centerLng,
+    double checkLat,
+    double checkLng,
+    double radius,
+  ) {
     final distance = Geolocator.distanceBetween(
       centerLat,
       centerLng,
